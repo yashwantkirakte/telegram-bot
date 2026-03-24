1566e44681c9e34c2480f1be8a904103a233a072
yashwantkirakte <yashwantkiraktee@gmail.com>
2026-03-24 23:08:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,22 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
+from telegram.ext import ApplicationBuilder, MessageHandler, CommandHandler, filters, ContextTypes
+import os
+import asyncio
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Welcome! 👋")
 
 async def reply_hi(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = update.message.text.lower()
     
     if text in ["hi", "hello"]:
-        await update.message.reply_text("""👋 Welcome to Stoic Ops!
-Your gateway to smart, strategic marketing discussions.
-We’re the official Telegram companion for Stoic Ops, a thriving Reddit community where marketers, entrepreneurs, and creators come together
-""")
+        await update.message.reply_text("Welcome! 👋")
 
-app = ApplicationBuilder().token("7952724991:AAF0RIQ8sTCNoxS0p1CZlPcvEDlvTswkv4U").build()
+app = ApplicationBuilder().token(TOKEN).build()
 
+app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.TEXT, reply_hi))
 
-app.run_polling()
+asyncio.run(app.run_polling())
